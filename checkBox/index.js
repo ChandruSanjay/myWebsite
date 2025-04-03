142f901c510e2216248255f4dc000a339e06c89e
Sanjay <chandrusanjay14@gmail.com>
2025-04-03 16:15:31 +0530

    added new javascript code

--- checkBox/index.js
+++ checkBox/index.js
@@ -8,23 +8,45 @@ const mySubmit = document.getElementById('mySubmit');
 
 
 mySubmit.onclick = ()=> {
+    
     if(checkBox.checked){
-        result.textContent = 'you are subscribed to this account'
+        if(checkBox.checked){
+            result.textContent = 'you are subscribed to this account'
+        }
+        else{
+            result.textContent = 'you are NOT subscribed!'
+        }
+        if(masterBtn.checked){
+            resultRadio.textContent = 'you are paying through MasterCard'
+        }
+        else if (visaBtn.checked){
+            resultRadio.textContent = 'you are paying through VisaCard'
+        }
+        else if(paypalBtn.checked) {
+            resultRadio.textContent = 'you are paying through Paypal'
+        }
+        else{
+           resultRadio.textContent= 'You need to select the payment way'
+        }
+    }else{
+         resultRadio.textContent= 'You must subscribe for do payment!'
     }
-    else{
-        result.textContent = 'you are NOT subscribed!'
-    }
+   
+     
+}
+let info;
 
-    if(masterBtn.checked){
-        resultRadio.textContent = 'you are paying through MasterCard'
-    }
-    else if (visaBtn.checked){
-        resultRadio.textContent = 'you are paying through VisaCard'
-    }
-    else if(paypalBtn.checked) {
-        resultRadio.textContent = 'you are paying through Paypal'
-    }
-    else{
-       resultRadio.textContent= 'You need to select  the payment way'
+let people =function(person,age){
+    this.person = person;
+    this.age = age;
+   this.info = function(){
+       console.log(this);
+        setTimeout(() =>{
+            console.log(this.person +"is" + this.age + "years old");
+        },3000)
     }
 }
+let person1 = new people('john',21);
+person1.info();
+
+
